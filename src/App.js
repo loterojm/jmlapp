819ff6d604425f0ebb28348b833da28ebdbcd058
jmlotero <jmlotero@gmail.com>
2022-08-31 23:58:47 -0300

    desafio de catalogo con map promeses con api de mercado libre

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Navbar from './components/Navbar';
-import ItemListContainer from './container/ItemListContainer';
+import ItemListCont from './container/ItemListCont';
 
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <ItemListContainer nombre="producto 1" descrip="Toda la descripcion del Producto 1"/>
-      <ItemListContainer nombre="producto 2" descrip="Toda la descripcion del Producto 2"/>
-      <ItemListContainer nombre="producto 3" descrip="Toda la descripcion del Producto 3"/>
-    </div>
+      <ItemListCont/>
+          </div>
   );
 }
 export default App;
---
+++ src/container/ItemListCont.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import ItemCount from "../components/ItemCount";
+
+const ItemList =({textodebusqueda}) => {
+const [carrito, setCarrito] = useState ([])
+const [productos,setProductos] = useState ([])
+
+const BuscarProductos = async () => {
+    try {
+        const response = await fetch (`https://api.mercadolibre.com/sites/MLA/search?q=celulares`)
+        const data = await response.json();
+        setProductos(data.results);
+    }catch(e){
+        console.log(e);
+    }
+}
+useEffect(() => {
+    BuscarProductos()
+},[textodebusqueda])
+console.log(productos)
+console.log("llego ok");
+
+return(
+    <div>
+    {productos.map((producto) => {
+        return(
+            <div>
+             <img src={producto.thumbnail}/>   
+            <h6>{producto.title}</h6>
+            <ItemCount stock={5} initial={1}/>
+            </div>
+        )
+    })}
+</div>            
+)
+      }
+      export default ItemList;
