9274c704f0f4cc9d5117e3d610e0e57335ecc08c
jmlotero <jmlotero@gmail.com>
2022-09-06 00:49:16 -0300

    agrego item

---
+++ src/container/Item.jsx
@@ -0,0 +1,13 @@
+import ItemCount from "../components/Item";
+
+const Item = ({ id }) => {
+    const addToCart = (id) => {
+
+    }
+    return (
+        <div>
+            {id}
+        </div>
+    )
+}
+export default Item;
